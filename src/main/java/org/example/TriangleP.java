4f124508039bb87ea07ece252a979d27a90cd22a
ibozorus <korkmazie@gmail.com>
2023-01-22 15:16:49 +0100

    -Initial Commit

--- src/main/java/org/example/TriangleP.java
+++ src/main/java/org/example/TriangleP.java
@@ -1,8 +1,33 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class TriangleP {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Basamak Sayisi: ");
+        int basamak = scanner.nextInt();
+        int tepeYildizSayisi = (basamak * 2) - 1;
+        String ucgen = "";
+        int yildizSayisi = tepeYildizSayisi;
+        int boslukSayisi = 0;
+        for (int z = 0; z < basamak; z++) {
+            for (int i = 0; i < boslukSayisi; i++) {
+                ucgen += " ";
+            }
+            for (int i = 0; i < yildizSayisi; i++) {
+                ucgen += "*";
+
+            }
+            for (int i = 0; i < boslukSayisi; i++) {
+                ucgen += " ";
+            }
+            ucgen += "\n";
+            yildizSayisi -= 2;
+            boslukSayisi++;
+        }
+        System.out.println(ucgen);
     }
 }
